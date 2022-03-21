2622d4ad48cdff9309506cfa39b876f994b0de03
JE-FH <5774453+JE-FH@users.noreply.github.com>
2022-03-21 10:15:36 +0100

    added buildsystem

---
+++ ShaellLang/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+
+
+namespace ShaellLang
+{
+    class ProgVisitor : ShaellBaseVisitor<object> {
+        public override object VisitProg(ShaellParser.ProgContext context)
+        {            
+            Console.WriteLine("Jeg besøger en prog");
+            return null;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1 ) {
+                Console.WriteLine("Expected first argument to be filename");
+                return;
+            }
+            var filename = args[0];
+
+            var content = File.ReadAllText(filename);
+
+            AntlrInputStream inputStream = new AntlrInputStream(content);
+            ShaellLexer shaellLexer = new ShaellLexer(inputStream);
+            CommonTokenStream commonTokenStream = new CommonTokenStream(shaellLexer);
+            ShaellParser shaellParser = new ShaellParser(commonTokenStream);
+            
+            ShaellParser.ProgContext progContext = shaellParser.prog();
+            var progVisitor = new ProgVisitor();
+            progVisitor.Visit(progContext);
+            
+        }
+    }
+}
