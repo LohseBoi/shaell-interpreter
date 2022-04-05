74eb0f8fb686b96d97249e2a0387275af3c6b1a2
Alexander Steffensen <74182126+alexsteffensen@users.noreply.github.com>
2022-04-05 10:20:14 +0200

    Implemented test (#57)
    * Implemeted test
    
    * Update README.md
    
    * forgot to add test project...
    
    * Update dotnet.yml
    
    * added .gitignore
    
    * Delete packages directory
    
    * Delete shaell-interpreter.sln.DotSettings.user
    
    * Delete global.json

--- ShaellLang/ShaellLang.cs
+++ ShaellLang/ShaellLang.cs
@@ -1,10 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Antlr4.Runtime;
+
 namespace ShaellLang
 {
-	public class Shaell
+	public class ShaellLang
 	{
-		public Shaell()
+		//Runs the test script and returns whether it failed or not
+		public bool Run(string file)
 		{
+			string fileContent = File.ReadAllText(file);
 
+			try
+			{
+				AntlrInputStream inputStream = new AntlrInputStream(fileContent);
+				ShaellLexer shaellLexer = new ShaellLexer(inputStream);
+				shaellLexer.AddErrorListener(new ShaellLexerErrorListener());
+				CommonTokenStream commonTokenStream = new CommonTokenStream(shaellLexer);
+				ShaellParser shaellParser = new ShaellParser(commonTokenStream);
+
+				ShaellParser.ProgContext progContext = shaellParser.prog();
+				var executer = new ExecutionVisitor();
+				executer.SetGlobal("$print", new NativeFunc(delegate(IEnumerable<IValue> args)
+				{
+					foreach (var value in args)
+					{
+						Console.Write(value.ToSString().Val);
+					}
+
+					Console.WriteLine();
+
+					return new SNull();
+				}, 0));
+
+				executer.SetGlobal("$T", TableLib.CreateLib());
+
+				executer.SetGlobal("$A", TestLib.CreateLib());
+
+				executer.SetGlobal("$debug_break", new NativeFunc(delegate(IEnumerable<IValue> args)
+				{
+					Console.WriteLine("Debug break");
+					return new SNull();
+				}, 0));
+
+				executer.Visit(progContext);
+			}
+			catch (SyntaxErrorException e)
+			{
+				Console.WriteLine(e.Message);
+				
+			}
+			return TestLib.testFailed;
 		}
 	}
 }
